6078a110755572160dfc8493cf917de10a4b1110
w3l <w3l@users.noreply.github.com>
2016-01-19 11:49:04 +0100

    +comments

--- holt45.php
+++ holt45.php
@@ -1,4 +1,9 @@
 <?
+/**
+ * Check $_GET
+ *
+ * @example if(chk_get("s") == "a") instead of if(isset($_GET["s"]) && $_GET["s"] == "a")
+ */
 function chk_get($key) {
     if (!isset($_GET[$key])) {
         return false;
@@ -6,6 +11,11 @@ function chk_get($key) {
     return $_GET[$key];
 }
 
+/**
+ * Check $_POST
+ *
+ * @example if(chk_post("s") == "a") instead of if(isset($_POST["s"]) && $_POST["s"] == "a")
+ */
 function chk_post($key) {
     if (!isset($_POST[$key])) {
         return false;
@@ -13,6 +23,11 @@ function chk_post($key) {
     return $_POST[$key];
 }
 
+/**
+ * Check multiple $_GET-keys
+ *
+ * @example if(chk_get_all(array("a","b"))) instead of if(!empty($_GET["a"]) && !empty($_GET["b"]))
+ */
 function chk_get_all($keys) {
 	$s = true;
 
@@ -25,6 +40,11 @@ function chk_get_all($keys) {
 	return $s;
 }
 
+/**
+ * Check multiple $_POST-keys
+ *
+ * @example if(chk_post_all(array("a","b"))) instead of if(!empty($_POST["a"]) && !empty($_POST["b"]))
+ */
 function chk_post_all($keys) {
 	$s = true;
 
