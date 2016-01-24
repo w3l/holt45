7b25b5f64fba8a5bd0e0e0b6a700c0a6607e33df
w3l <w3l@users.noreply.github.com>
2016-01-24 14:43:20 +0100

    visibility for rgbhex

--- holt45/Convert.php
+++ holt45/Convert.php
@@ -8,7 +8,7 @@ trait Convert {
 	 * @param array $rgb RGB color
 	 * @return string Hexadecimal color
 	 */
-	function rgbhex($rgb) {
+	public static function rgbhex($rgb) {
 		$hex = "";
 		foreach($rgb AS $color) {
 			$hex .= str_pad(dechex($color), 2, "0", STR_PAD_LEFT);
